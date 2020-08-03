3eb22ac0f41899d3c2711f30ba96e3a0bdda21b3
Ron <laniado.ron@gmail.com>
2020-08-02 22:41:00 -0400

    fixed bug to make the game actually run

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-#import your controller
+from src import controller
 import pygame
 
 def main():
     pygame.init()
     print("Software Lead is: Ron Laniado")
     print("Backend is: David Esses")
-    print("Frontend is: Ron Laniado")
-
+    print("Frontend is: Matthew Sadowski")
+    window = controller.Controller()
+    window.mainLoop()
 main()
